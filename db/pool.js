7f6643c497010316f207ab51a4c49a189df99b50
Ibrahim Ajani <saturnsteed@gmail.com>
2025-08-29 16:52:48 +0100

    fixed a secret leak

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,6 @@ const { Pool } = require("pg");
 
 dotenv.config(); // load environment variables
 
-// const connectionString =
-//     "postgresql://minimessage_db_user:d49SSTd2AXiPgjTjaFeGh9OIbpw20Vdr@dpg-d2oom224d50c73a6tsn0-a.oregon-postgres.render.com/minimessage_db";
-
 module.exports = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
